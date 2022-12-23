03b2f578dee2ffb9a9ee42c8e444bb0a284d3aa1
Maksim.Surimov <MaxxLorge@gmail.com>
2022-12-23 18:55:31 +0500

    initial commit

---
+++ SheetEditor/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Configuration;
+
+var configurationRoot = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+ //   .AddJsonFile("secrets.json")
+    .Build();
+
+var test = configurationRoot["Test"];
+Console.ReadLine();
+    
