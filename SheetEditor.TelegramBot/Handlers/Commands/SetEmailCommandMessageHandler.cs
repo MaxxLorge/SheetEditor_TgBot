e6d573c6f3e507080df8f3274f127fcbf0f06848
Maksim.Surimov <MaxxLorge@gmail.com>
2022-12-24 23:10:47 +0500

    SetEmailCommandMessageHandler рефакторинг

--- SheetEditor.TelegramBot/Handlers/Commands/SetEmailCommandMessageHandler.cs
+++ SheetEditor.TelegramBot/Handlers/Commands/SetEmailCommandMessageHandler.cs
@@ -3,60 +3,35 @@ using SheetEditor.Data;
 using SheetEditor.Handlers.Abstractions;
 using Telegram.Bot;
 using Telegram.Bot.Types;
-using User = SheetEditor.Data.Entities.User;
 
 namespace SheetEditor.Handlers.Commands;
 
-public class SetEmailCommandMessageHandler : ICommandMessageHandler
+public class SetEmailCommandMessageHandler : CommandMessageHandlerBase
 {
-    private readonly SheetEditorContext _context;
-
-    public SetEmailCommandMessageHandler(SheetEditorContext context)
+    public SetEmailCommandMessageHandler(SheetEditorContext context) : base(context)
     {
-        _context = context;
     }
 
-    public string MessageKey => "/setEmail";
+    public override string MessageKey => "setEmail";
 
-    public async Task Process(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    public override async Task Handle(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
-        var message = update.Message;
-        var messageText = message?.Text;
-        var chatId = message?.Chat.Id;
-        if (messageText == null || chatId == null)
-            throw new InvalidOperationException();
-
-        var splited = messageText.Split(' ', StringSplitOptions.TrimEntries);
-        if (splited.Length != 2)
+        if (MessageWords.Length != 2)
         {
-            await botClient.SendTextMessageAsync(chatId,
+            await botClient.SendTextMessageAsync(ChatId,
                 "Неверный формат сообщения",
                 cancellationToken: cancellationToken);
             return;
         }
 
-        var email = splited[1];
+        var email = MessageWords[1];
 
-        var dbUser = await _context.Users
-            .FirstOrDefaultAsync(e => e.TelegramId == message!.From!.Id, cancellationToken);
-        if (dbUser == null)
-        {
-            _context.Users.Add(new User
-            {
-                TelegramId = message.From.Id,
-                Email = email
-            });
-        }
-        else
-        {
-            dbUser.Email = email;
-            _context.Update(dbUser);
-        }
+        var dbUser = await Context.Users
+            .FirstOrDefaultAsync(e => e.TelegramId == TelegramUser.Id, cancellationToken);
+        dbUser.Email = email;
+        await Context.SaveChangesAsync(cancellationToken);
 
-        await _context.SaveChangesAsync(cancellationToken);
-
-        await botClient.SendTextMessageAsync(chatId,
-            "Почта была успешно установлена. Чтобы сменить почту, введите команду еще раз",
+        await SendMessage("Почта была успешно установлена. Чтобы сменить почту, введите команду еще раз",
             cancellationToken: cancellationToken);
     }
 }
